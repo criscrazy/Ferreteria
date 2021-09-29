0d4a13d30e270c4f3e7267a20509d172c83776da
CrisCrazy <criscrazy133@gmail.com>
2021-09-29 00:07:34 -0600

    updating 3.0

--- apps/Ferre/views.py
+++ apps/Ferre/views.py
@@ -4,4 +4,8 @@ from .models import *
 def index(request):
     allproduct = producto.objects.all()
     return render(request, 'index.html', {'allproduct':allproduct})
+
+def contact(request):
+    return render(request, 'contact.html')
 # Create your views here.
+
