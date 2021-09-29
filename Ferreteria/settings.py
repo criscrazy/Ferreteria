241d210545556202c5c75a6a4e634f62d1c76e01
CrisCrazy <criscrazy133@gmail.com>
2021-09-29 09:59:17 -0600

    changes en index

--- Ferreteria/settings.py
+++ Ferreteria/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'Ferreteria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
