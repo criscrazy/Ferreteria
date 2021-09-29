74f7d3c782a291d76170e495339b6490e97f7244
CrisCrazy <criscrazy133@gmail.com>
2021-09-28 22:38:10 -0600

    New database

--- Ferreteria/settings.py
+++ Ferreteria/settings.py
@@ -74,12 +74,21 @@ WSGI_APPLICATION = 'Ferreteria.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+"""
+import dj_database_url
+from decouple import config
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
