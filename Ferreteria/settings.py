195b49029dc33028a022a88bee406996cbdf7c45
CrisCrazy <criscrazy133@gmail.com>
2021-09-28 22:25:48 -0600

    update setings.py

--- Ferreteria/settings.py
+++ Ferreteria/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$0apna5=a32on7rbrh#xwaahk+c))i8$$*e)rpcxua2x%xn@0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
